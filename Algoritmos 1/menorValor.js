019a3939e7cf6af0bf2f49b0893a8c4147f94ced
Luisa Oliveira <64452974+luisaoli@users.noreply.github.com>
2022-04-04 15:43:49 -0300

    aula 2
    ordenação da array de livros

---
+++ Algoritmos 1/menorValor.js
@@ -0,0 +1,18 @@
+//obj: encontrar o livro mais barato e o mais caro dentro do array
+
+const livros = require('./lista-livros');
+
+function menorValor(arrProdutos, posicaoInicial){
+    let maisBarato = posicaoInicial;
+    
+    for (let atual = posicaoInicial; atual < arrProdutos.length; atual++){
+        if (arrProdutos[atual].preco < arrProdutos[maisBarato].preco){
+            maisBarato = atual;
+        }
+    }
+
+    return maisBarato;
+}
+
+
+module.exports = menorValor;
